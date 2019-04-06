efeea4428ac4383ec75b0d4aae72f0f53a453fbf
Neznakomec <vladimirsukko1999@mail.ru>
2019-04-06 21:27:21 +0300

    Added import to xml and csv files

--- import.py
+++ import.py
@@ -1,35 +1,47 @@
 import argparse
 import psycopg2
-import xml.etree.ElementTree as xml
+import csv
 from config import DB
 
+EXTENTIONS={'xml':'.xml','csv':'.csv', 'sql':'.sql', 'html':'.html'}
+
+def write_to_csv(filename, stream):
+    with open(filename,'w') as f:
+        writer = csv.writer(f,delimiter=';')
+        res=stream.fetchone()
+        if filename=='Cookies.csv':
+            writer.writerow(('cookie','source url'))
+        elif len(res)>1 and filename=='Urls.csv':
+            writer.writerow(('url','source url;'))
+        else:
+            writer.writerow(('url'))
+        while res:
+            print(res)
+            writer.writerow(res)
+            res=stream.fetchone()
 
 
-def write_to_xml(Data_type, res):
-    filename = Data_type.title() +'.xml'
-    print(res)
-    if Data_type=='cookies':
-        sourse='url'
-        D_type='cookie'
-    elif Data_type=='urls':
-        sourse='source url'
-        D_type='url'
-
-    root = xml.Element('z'+Data_type.title())
-    '''appt = xml.Element(Data_type)    
-    root.append(appt)'''
-
-    cookie = xml.SubElement(root, D_type)
-    cookie.text=res[0]+'\n'+'\t'
-    if len(res)>1:
-        srse = xml.SubElement(root, sourse)
-        srse.text=res[1]+'\n'
-
-    tree = xml.ElementTree(root)
-    with open(filename, "ab") as fh:
-        tree.write(fh)
-        fh.write(b'\n')
-
+def write_to_xml(filename, stream):
+    data = filename[:-5]
+    source='source_url'
+    with open(filename,'w') as f:
+        f.write('<zCookies>\n\t')
+        res=stream.fetchone()
+        while res:
+            print(res)
+            s='<'+data+'>'+res[0]+'</'+data
+            if len(res)>1:
+                so='<'+source+'>'+res[1]+'</'+source+'>\n\t'  
+            s=('%r'%s)[1:-1]
+            s+='>\n\t'
+            try:
+                f.write(s)
+                if len(res)>1:
+                    f.write(so)
+            except:
+                pass
+            res=stream.fetchone()
+        
 
 def select_cookie_from_DB(type_of_file):
     conn = psycopg2.connect(host=args.address, user=DB['username'], password=DB['password'], dbname=DB['dbname'])   
@@ -37,29 +49,27 @@ def select_cookie_from_DB(type_of_file):
 
     c.execute('SELECT cookie,url FROM cookies;')
     res=c.fetchone()
-    while res:
-        write_to_xml('cookies',[res[0],res[1]])
-        res=c.fetchone()
-        
-    return print('-------------------Cookies_writing_completed-------------------')
+    filename='Cookies'+EXTENTIONS[type_of_file]
+    if type_of_file=='xml':
+        write_to_xml(filename,c)
+
+    elif type_of_file=='csv':
+        write_to_csv(filename,c)
+
+
+
 
 def select_url_from_DB(type_of_file, need_sourse=False):
     conn = psycopg2.connect(host=args.address, user=DB['username'], password=DB['password'], dbname=DB['dbname'])   
     c = conn.cursor()
     if need_sourse:
         c.execute('SELECT url,url_source FROM urls;')
-        res=c.fetchone()
-        while res:
-            write_to_xml('urls',[res[0],res[1]])
-            res=c.fetchone()
     else:
         c.execute('SELECT url FROM urls;')
-        res=c.fetchone()
-        while res:
-            write_to_xml('urls',[res[0]])
-            res=c.fetchone()
-        
-    return print('-------------------URL_writing_completed-------------------')
+    if type_of_file=='csv':
+        write_to_csv('Urls.csv',c)
+    elif type_of_file=='xml':
+        write_to_xml('Urls.xml',c)
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser(description='Import/Export info from DB to some files')
@@ -71,4 +81,12 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--address', type=str, help='default ip address is 127.0.0.1',
                         default='127.0.0.1', metavar='')
     args=parser.parse_args()
+
     select_url_from_DB('xml',need_sourse=True)
+    if args.url==1:
+        select_url_from_DB(args.extention,need_sourse=False)
+    elif args.url>1:
+        select_url_from_DB(args.extention,need_sourse=True)
+    
+    if args.cookie>1:
+        select_cookie_from_DB(args.extention)
